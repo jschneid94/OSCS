de763c1b13986991f9c6f7ffee182e119edb3d68
Enea <eneasecondary@gmail.com>
2018-07-20 16:51:00 -0700

    Added functionality to the API buttons, they also have a spot for roger to inject his content. Radio buttons are all named and grabbable, temp footer at the bottom displays moment js current time.

---
+++ assets/javascript/EGA_logoDisplays.js
@@ -0,0 +1,69 @@
+$("#EGA_youtubeApiLogo").on('click', function () {
+
+  if ($("#EGA_youtubeTitleLogo").hasClass("hidden")) {
+
+    $("#EGA_youtubeTitleLogo").removeClass("hidden");
+    $("#EGA_youtubeTitleLogo").addClass("visible");
+
+  } else if ($("#EGA_youtubeTitleLogo").hasClass("visible")) {
+
+    $("#EGA_youtubeTitleLogo").removeClass("visible");
+    $("#EGA_youtubeTitleLogo").addClass("hidden");
+  }
+});
+
+$("#EGA_stackoverflowApiLogo").on('click', function () {
+
+  if ($("#EGA_stackoverflowLogo").hasClass("hidden")) {
+
+    $("#EGA_stackoverflowLogo").removeClass("hidden");
+    $("#EGA_stackoverflowLogo").addClass("visible");
+
+  } else if ($("#EGA_stackoverflowLogo").hasClass("visible")) {
+
+    $("#EGA_stackoverflowLogo").removeClass("visible");
+    $("#EGA_stackoverflowLogo").addClass("hidden");
+  }
+});
+
+$("#EGA_amazonApiLogo").on('click', function () {
+
+  if ($("#EGA_amazonLogo").hasClass("hidden")) {
+
+    $("#EGA_amazonLogo").removeClass("hidden");
+    $("#EGA_amazonLogo").addClass("visible");
+
+  } else if ($("#EGA_amazonLogo").hasClass("visible")) {
+
+    $("#EGA_amazonLogo").removeClass("visible");
+    $("#EGA_amazonLogo").addClass("hidden");
+  }
+});
+
+$("#EGA_githubApiLogo").on('click', function () {
+
+  if ($("#EGA_githubLogo").hasClass("hidden")) {
+
+    $("#EGA_githubLogo").removeClass("hidden");
+    $("#EGA_githubLogo").addClass("visible");
+
+  } else if ($("#EGA_githubLogo").hasClass("visible")) {
+
+    $("#EGA_githubLogo").removeClass("visible");
+    $("#EGA_githubLogo").addClass("hidden");
+  }
+});
+
+$("#EGA_spotifyApiLogo").on('click', function () {
+
+  if ($("#EGA_spotifyLogo").hasClass("hidden")) {
+
+    $("#EGA_spotifyLogo").removeClass("hidden");
+    $("#EGA_spotifyLogo").addClass("visible");
+
+  } else if ($("#EGA_spotifyLogo").hasClass("visible")) {
+
+    $("#EGA_spotifyLogo").removeClass("visible");
+    $("#EGA_spotifyLogo").addClass("hidden");
+  }
+});
