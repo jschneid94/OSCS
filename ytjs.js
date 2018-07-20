ab610fa3342efeb87b7dd80cb36d29175979d925
Roger <rdpender3@gmail.com>
2018-07-20 00:08:57 -0700

    got stuff working

--- ytjs.js
+++ ytjs.js
@@ -2,60 +2,56 @@
 $("form").on("click", "#rdp_makeVideo", function (e) {
     e.preventDefault();
 
-var youtubeApiKey = "AIzaSyDZ4WJqvmeaEhTRbGq9uNHPYjUy4pnlsn8";
+    var youtubeApiKey = "AIzaSyDZ4WJqvmeaEhTRbGq9uNHPYjUy4pnlsn8";
 
-var queryURL = "https://www.googleapis.com/youtube/v3/search"
+    var queryURL = "https://www.googleapis.com/youtube/v3/search"
 
-var options = {
-    part: "snippet",
-    key: youtubeApiKey,
-    type: "video",
-    q: encodeURIComponent($("#rdp_search").val()).replace(/%20/g, "+"),
-    maxResults: 5,
-    order: "viewCount",
-    relevanceLanguage: "en",
-    regionCode: "US",
-    publishedAfter: "2016-01-01T00:00:00Z"
-}
+    var options = {
+        part: "snippet",
+        key: youtubeApiKey,
+        type: "video",
+        q: encodeURIComponent($("#rdp_search").val()).replace(/%20/g, "+"),
+        maxResults: 5,
+        order: "viewCount",
+        relevanceLanguage: "en",
+        regionCode: "US",
+        publishedAfter: "2016-01-01T00:00:00Z"
+    }
 
-$('#rdp_smallerVids').empty();
-loadVids();
+    $('#rdp_smallerVids').empty();
+    loadVids();
 
-function loadVids() {
-    $.getJSON(queryURL, options, function(data){
-        console.log(data, "data");
-        var id = data.items[0].id.videoId;
-        mainVid(id);
-        resultsLoop(data);
-    })
-}
+    function loadVids() {
+        $.getJSON(queryURL, options, function (data) {
+            console.log(data, "data");
+            var id = data.items[0].id.videoId;
+            mainVid(id);
+            resultsLoop(data);
+        })
+    }
 
-  
-function mainVid(id) {
-    
-    $("#rdp_videosHere").html(`
+    function mainVid(id) {
+
+        $("#rdp_videosHere").html(`
     <iframe width="560" height="315" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
     `)
-};
+    };
 
-function resultsLoop(data) {
+    function resultsLoop(data) {
 
         for (var i = 1; i < data.items.length; i++) {
-        var vid = data.items[i].id.videoId;
-        var newDiv = $("<article class='item' data-key='" + vid + "'>");
-        var thumb = $("<a href='https://www.youtube.com/embed/" + vid + "' target ='_blank'><img src='" + data.items[i].snippet.thumbnails.medium.url + "' alt='' class='thumb'></a>");
-        var title = $("<h4>" + data.items[i].snippet.title + "</h4>");
-        var desc = $("<p>" + data.items[i].snippet.description.substring(0, 100) + "</p>");
+            var vid = data.items[i].id.videoId;
+            var newDiv = $("<article class='item' data-key='" + vid + "'>");
+            var thumb = $("<a href='https://www.youtube.com/embed/" + vid + "' target ='_blank'><img src='" + data.items[i].snippet.thumbnails.medium.url + "' alt='' class='thumb'></a>");
+            var title = $("<h4>" + data.items[i].snippet.title + "</h4>");
+            var desc = $("<p>" + data.items[i].snippet.description.substring(0, 100) + "</p>");
+
+            newDiv.append(thumb, title, desc);
 
 
-        
-        newDiv.append(thumb, title, desc);
-
-
-        $('#rdp_smallerVids').append(newDiv);
+            $('#rdp_smallerVids').append(newDiv);
         }
 
-
-}
+    }
 
 });
