f1dd5fed1f9932e8aa4765ef1ddf1c013e96ab51
Roger <rdpender3@gmail.com>
2018-07-19 23:08:27 -0700

    fixed some js

--- ytjs.js
+++ ytjs.js
@@ -1,14 +1,11 @@
 
-$("form").on("submit", function (e) {
+$("form").on("click", "#rdp_makeVideo", function (e) {
     e.preventDefault();
 
 var search = $("#rdp_search").val();
 console.log(search, "search");
 
 var youtubeApiKey = "AIzaSyDZ4WJqvmeaEhTRbGq9uNHPYjUy4pnlsn8";
-
-var player;
-
 
 var queryURL = "https://www.googleapis.com/youtube/v3/search"
 
@@ -26,29 +23,18 @@ loadVids();
 
 function loadVids() {
     $.getJSON(queryURL, options, function(data){
-        console.log(data);
-        var video = data.items[0].id.videoID;
-        console.log(video, "video")
-        mainVid(video);
-        //resultsLoop(data);
-        onYouTubeIframeAPIReady(video);
+        console.log(data, "data");
+        var id = data.items[0].id.videoId;
+        // console.log(data.items[0].id.videoID);
+        console.log(id, "id")
+        mainVid(id);
+        resultsLoop(data);
+    })
+}
         
+{/* <iframe width="1483" height="683" src="https://www.youtube.com/embed/yfoY53QXEnI" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe> */}
 
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player('rdp_player', {
-    height: '390',
-    width: '640',
-    videoId: video,
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
-    }
-  });
-}
-
-    });
-}
-
+  
 function mainVid(id) {
     
     $("#rdp_videosHere").html(`
@@ -56,60 +42,32 @@ function mainVid(id) {
     `)
 };
 
-// function resultsLoop(data) {
+function resultsLoop(data) {
 
-//     $.each(data.items, function (i, item) {
+    $.each(data.items, function (i, item) {
 
-//         var thumb = item.snippet.thumbnails.medium.url;
-//         var title = item.snippet.title;
-//         var desc = item.snippet.description.substring(0, 100);
-//         var vid = data.items[i].id.videoID;
+        var thumb = item.snippet.thumbnails.medium.url;
+        var title = item.snippet.title;
+        var desc = item.snippet.description.substring(0, 100);
+        var vid = data.items[i].id.videoId;
+        console.log(thumb, "thumb")
+        console.log(title, "title")
+        console.log(desc, "vid")
+        console.log(vid, "vid")
 
-//         $('#rdp_smallerVids').append(`
-//                         <article class="item" data-key="${vid}">
+        $('#rdp_smallerVids').append(`
+                        <article class="item" data-key="${vid}">
 
-//                             <img src="${thumb}" alt="" class="thumb">
-//                             <div class="details">
-//                                 <h4>${title}</h4>
-//                                 <p>${desc}</p>
-//                             </div>
+                            <img src="${thumb}" alt="" class="thumb">
+                            <div class="details">
+                                <h4>${title}</h4>
+                                <p>${desc}</p>
+                            </div>
 
-//                         </article>
-//                     `);
-//     });
+                        </article>
+                    `);
+    });
 
-// }
-
-
-
-
-var tag = document.createElement('script');
-
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-// 3. This function creates an <iframe> (and YouTube player)
-//    after the API code downloads.
-
-
-// 4. The API will call this function when the video player is ready.
-function onPlayerReady(event) {
-  event.target.playVideo();
-}
-
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
-var done = false;
-function onPlayerStateChange(event) {
-  if (event.data == YT.PlayerState.PLAYING && !done) {
-    setTimeout(stopVideo, 6000);
-    done = true;
-  }
-}
-function stopVideo() {
-  player.stopVideo();
 }
 
 });
