c08c95144a495c3db29aa18ece4e0cf70cd64847
Roger <rdpender3@gmail.com>
2018-07-21 13:19:56 -0700

    ready to push to master

--- ytjs.js
+++ ytjs.js
@@ -1,20 +1,13 @@
 // to do
-// add classes to elements and add styles to css doc
 // try and get russian links to stop showing up
-// clean up main vid function
-// need to change video content
-// center text
-// make description above links
+
 // need to make the search recognize the radio button clicked
 
-
-
-// add class to all results called results
-
-
+// click event that calls youtubeAPI 
 $("form").on("click", "#RDP_makeVideo", function (e) {
     e.preventDefault();
 
+    // parameters for actual youtube call
     var youtubeApiKey = "AIzaSyDZ4WJqvmeaEhTRbGq9uNHPYjUy4pnlsn8";
 
     var queryURL = "https://www.googleapis.com/youtube/v3/search"
@@ -31,9 +24,13 @@ $("form").on("click", "#RDP_makeVideo", function (e) {
         publishedAfter: "2016-01-01T00:00:00Z"
     }
 
+    // empties small div so it doesn't keep appending
     $('#RDP_smallerVids').empty();
+
+    // call load div function
     loadVids();
 
+    // function that acutally calls youtubeAPI
     function loadVids() {
         $.getJSON(queryURL, options, function (data) {
             console.log(data, "data");
@@ -43,11 +40,12 @@ $("form").on("click", "#RDP_makeVideo", function (e) {
         })
     }
 
+    // function making the larger video on screen and formatting it
     function mainVid(data) {
         var id = data.items[0].id.videoId;
 
         var mainDiv = $("<div class='results' id='RDP_mainDiv'></div>");
-        
+
         var vidDiv = $("<iframe class='RDP_iframeSize' src='https://www.youtube.com/embed/" + id + "frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe>");
 
         var descDiv = $("<div class='row'></div>");
@@ -58,17 +56,18 @@ $("form").on("click", "#RDP_makeVideo", function (e) {
         mainDiv.append(vidDiv, descDiv);
 
         $("#RDP_videosHere").html(mainDiv);
-    
+
     };
 
+    // loops in the smaller thumbnails
     function resultsLoop(data) {
 
         $("#RDP_thumbDesc").html("Click a thumbnail below to watch a video");
 
         for (var i = 1; i < data.items.length; i++) {
-            
+
             var vid = data.items[i].id.videoId;
-            
+
             var newDiv = $("<div class='RDP_item' data-key='" + vid + "'></div>");
             var thumb = $("<a href='https://www.youtube.com/embed/" + vid + "' target ='_blank'><img src='" + data.items[i].snippet.thumbnails.high.url + "' alt='' class='RDP_thumb'></a>");
 
