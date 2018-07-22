e72bc4976117c8bc01c24806b9025dce73e643d2
elaine <user@host.com>
2018-07-22 16:13:11 -0700

    favorite appendations and starting to get local storage

--- esh_app.js
+++ esh_app.js
@@ -1,32 +1,49 @@
+var esh_stackcounter = 0;
+var esh_ytcounter = 0;
+var esh_ytArr = [];
+var esh_stackArr = [];
+
 $(document).ready(function () {
 
-    var esh_favoritescounter = 0;
-
-    // display search results as object
-    // youtube array
-    // display each in a div
-    // access div by class/id
-
     // on any result click, append to favorites section
-    $(document).on("click", "results:button", function () {
+    $(document).on("click", ".favorite", function () {
         //$(this) needs to be the whole search div
         console.log($(this).parent()[0]);
-        var esh_resulttomove = $(this).parent()[0];
-        $(this).remove();
-        $("#esh_favorites").append(esh_resulttomove);
 
-        // save to local storage
-        localStorage.setItem("favorites-" + esh_favoritescounter, esh_resulttomove);
-        esh_favoritescounter++;
+        esh_ytArr = [];
+        esh_stackArr = [];
 
+        // youtube - save url and embed thumbnail into favorites section
+        if ($(this).parents("#RDP_videosHere").length > 0) {
+            var esh_ytthumbnail = $(".RDP_item").attr("data-key");
+            var esh_yttitle = $(".RDP_titlediv").attr("data-key");
+
+            // change button to favorited
+
+            // thumbnail dimensions: 480w x 360h
+            $("#esh_favorites").append("<img width='100%' src='" + esh_ytthumbnail + "'>", esh_yttitle);
+
+            // save to local storage
+            esh_ytArr.push(esh_ytthumbnail, esh_yttitle);
+            localStorage.setItem("ytFav-" + esh_ytcounter, esh_ytArr);
+            esh_ytcounter++;
+
+            // if stackoverflow set as obj with views, score, title, stackoverflow link
+            // stack exchange - retrieve data name and url to save to local storage
+        } else if ($(this).parents(".EGA_stackoverflowContainer").length > 0) {
+            var esh_stackresult = $(this).parent()[0];
+            esh_stackArr.push($(".card-title").attr("data-title"), $(".card-title").attr("data-link"));
+            localStorage.setItem("stackFav-" + esh_stackcounter, JSON.stringify(esh_stackArr));
+            esh_stackcounter++;
+
+            // change button to favorited
+
+            $("#esh_favorites").append(esh_stackresult);
+        }
 
 
     });
 
     // how to get local storage to load on site
-    // youtube - save url and embed thumbnail into favorites section
-    // stack exchange - retrieve data name and url to display in favorites section
-    // save as object if stack exchange
-
 
 });
